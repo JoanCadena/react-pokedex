173b313c085b3178483ac3f99c735d0b73204e01
joancadena <inmortal_20@live.com>
2025-03-21 16:20:52 -0500

    fix: Remove speed insights

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,6 @@
 import { JSX } from "react";
 import styles from "../styles/layout.module.scss";
 import Header from "./Header";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 interface ILayout {
   children: JSX.Element;
@@ -10,7 +9,6 @@ interface ILayout {
 const Layout = ({ children }: ILayout) => {
   return (
     <div className={styles.layout}>
-      <SpeedInsights />
       <Header />
       {children}
     </div>
